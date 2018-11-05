16925a4d23c627863238ba5186f63ff8c3088155
Jon Gray <jgray@spartaglobal.com>
2018-11-05 07:04:43 +0000

    v0.7 comments sections completed. 3 errors remain: delete functionality, index only displays one post and comments are invisible with dysfunctional link.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "brad", password:"1234", except: [:index, :show]
+  #
   def index
     @posts = Post.all
   end
-
+  #
   def show
     @post = Post.find(params[:id])
   end
-
+  #
   def new
     @post = Post.new
   end
-
+  #
   def create
     #render plain: params[:post].inspect
     @post = Post.new(post_params)
@@ -21,7 +23,7 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
-
+  #
   def edit
     @post = Post.find(params[:id])
   end
@@ -34,13 +36,13 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
-
-  def destroy
-    @post = Post.find(params[:id)])
-    @post.destroy
-    redirect_to posts_path
-  end
-
+  #
+  # def destroy
+  #   @post = Post.find(params[:id)])
+  #   @post.destroy
+  #   redirect_to posts_path
+  # end
+  #
   private def post_params
     params.require(:post).permit(:title, :body)
   end
